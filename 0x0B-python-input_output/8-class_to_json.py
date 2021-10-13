5e0a2eb6eec91bfa150a9c5d6bd4337a83b9b506
DavidGondo <d.gondo@alustudent.com>
2021-10-12 23:27:07 -0700

    Update

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""json object"""
+
+
+def class_to_json(obj):
+    """dict obj"""
+    return obj.__dict__#!/usr/bin/python3
+'''function that returns the dictionary description with simple data structure
+'''
+
+
+def class_to_json(obj):
+    '''module class_to_json
+       returns builds a dictionary
+    '''
+    return obj.__dict__
